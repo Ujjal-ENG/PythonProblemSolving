3fa3192dd27fc33b1752fa136b5fb4b2352df5a0
user <ujjalroy7862@gmail.com>
2022-10-16 00:54:34 +0600

    firstDay first project done in pythony

--- 1stDay.py
+++ 1stDay.py
@@ -1,2 +1,14 @@
-a = input("What is your name: ")
-print(a)
+#1. Create a greeting for your program.
+print("Welcome to the band Name generator System")
+
+#2 Ask the user for the city that they grew up in.
+userCity = input("Which city did you grow up in?")
+
+#3 Ask the user for the name of a pet.
+userPet = input("What is the name of a pet?")
+
+#4 Combine the name of their city and pet and show them their band name.
+bandName = userCity + userPet
+
+print(f"Your Band Name could Be: {bandName}\n")
+#5 Make sure the input cursor shows on a new line, see the example at: 
