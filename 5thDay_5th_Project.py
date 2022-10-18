7dcd3f2564ae6df51c1122794e6f7a74379f3318
user <ujjalroy7862@gmail.com>
2022-10-18 14:48:58 +0600

    For loop Checking from list of a Numbers

--- 5thDay_5th_Project.py
+++ 5thDay_5th_Project.py
@@ -1,13 +1,23 @@
 # 
 
-studentNumber = input("Enter the numbers of class test of the students: ").split(',')
+# studentNumber = input("Enter the numbers of class test of the students: ").split(',')
 
-for number in range(0, len(studentNumber)):
-  studentNumber[number] = int(studentNumber[number])
+# for number in range(0, len(studentNumber)):
+#   studentNumber[number] = int(studentNumber[number])
 
-max = 0
-for eachNumber in studentNumber:
-  if max < eachNumber:
-    max = eachNumber
-print(max)
+# max = 0
+# for eachNumber in studentNumber:
+#   result = max(eachNumber)
+# print(max)
+
+for number in range(1,16):
+  if number % 3 ==0:
+    print('Fizz')
+  elif number % 5 ==0:
+    print('Buzz')
+  elif number % 3 == 0 and number % 5 == 0 :
+    print('FizzBuzz')
+  else:
+    print(number)
+
 
