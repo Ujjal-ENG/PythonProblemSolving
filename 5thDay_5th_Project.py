aded151209cfd1e1617aeb3c2c9a5ca829c5afe2
ujjal_roy <ujjalroy7862@gmail.com>
2025-02-12 23:01:54 +0600

    added

--- 5thDay_5th_Project.py
+++ 5thDay_5th_Project.py
@@ -3,8 +3,7 @@
 # studentNumber = input("Enter the numbers of class test of the students: ").split(',')
 
 # for number in range(0, len(studentNumber)):
-#   studentNumber[number] = int(studentNumber[number])
-
+#   studentNumber[number] = int(studentNumber[number]
 # max = 0
 # for eachNumber in studentNumber:
 #   result = max(eachNumber)
